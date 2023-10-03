9cb379908f01fadf74635941d4fdc96cbabfe513
Utkarsh Bareja <utkarshpant029@gmail.com>
2023-10-03 23:47:55 +0530

    initial

--- src/app/home/home-view/home-view.component.ts
+++ src/app/home/home-view/home-view.component.ts
@@ -9,6 +9,23 @@ import * as $ from 'jquery'
 export class HomeViewComponent implements OnInit {
 
   ngOnInit(): void {
+
+
+    $(document).ready(function(){
+      var mouseX, mouseY;
+      var ww = $( window ).width();
+      var wh = $( window ).height();
+      var traX, traY;
+      $(document).mousemove(function(e){
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+        traX = ((4 * mouseX) / 570) + 40;
+        traY = ((4 * mouseY) / 570) + 50;
+        console.log(traX);
+        $(".title2").css({"background-position": traX + "%" + traY + "%"});
+      });
+    });
+    
     
     /*$(document).ready(function () {
 
